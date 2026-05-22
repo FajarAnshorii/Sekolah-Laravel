efd8e82e978434ad07a1b08713aecdf608194807
Muhammad Fajar Anshori <fajariphone2506@gmail.com>
2026-05-22 23:16:44 +0700

    chore: optimize dynamic Nginx port script to safely target specific config files

--- docker/99-set-nginx-port.sh
+++ docker/99-set-nginx-port.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 if [ -n "$PORT" ]; then
     echo "Dynamic PORT environment variable detected: $PORT"
-    # Find all files in Nginx config and replace the default 8080 with the dynamic $PORT
-    find /etc/nginx -type f -exec sed -i "s/8080/$PORT/g" {} +
+    # Target only specific configuration files containing the listen port to be extremely safe
+    for file in /etc/nginx/nginx.conf /etc/nginx/site-opts.d/http.conf /etc/nginx/site-opts.d/http.conf.template; do
+        if [ -f "$file" ]; then
+            echo "Updating Nginx port in $file..."
+            sed -i "s/8080/$PORT/g" "$file"
+        fi
+    done
 else
     echo "No PORT environment variable defined, defaulting Nginx to port 8080"
 fi
